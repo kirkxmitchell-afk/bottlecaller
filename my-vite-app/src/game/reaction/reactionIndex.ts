1a4f034e902dbe8972ac5948be9bab01b2f70c60
XxhuntlyxX <jason.huntly.grobler@gmail.com>
2026-03-27 17:04:25 +0200

    Revert to pre-Netlify project state

--- my-vite-app/src/game/reaction/reactionIndex.ts
+++
@@ -1,4 +0,0 @@
-export * from "./reactionTypes";
-export * from "./reactionBuilders";
-export * from "./reactionRuntime";
-export * from "./reactionSelectors";
--- my-vite-app/src/lib/supabaseSingleton.js
+++
@@ -1,3 +0,0 @@
-window.__BC_SUPABASE_SINGLETON_TOUCHED__ = true;
-
-export { getSupabaseParent as getSupabase } from "./supabaseParent.js";
